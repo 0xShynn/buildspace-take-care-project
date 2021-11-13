552fcc096ed4a1fc43a2cabd37803e3b7a32f366
Antonin Nhek <a.nhek@icloud.com>
2021-11-13 17:59:24 +0100

    section 1 completed :  building the quote grid

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import { useEffect, useState } from 'react';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_QUOTESS = [
+  'https://i.ibb.co/z2m4j94/Image-about-quotes-in-w-o-r-d-s-by-i-m-p-e-r-f-e-c-t-i-o-n.png',
+  'https://i.ibb.co/888FWnC/image.png',
+  'https://i.ibb.co/n1fKrTP/image.jpg',
+  'https://i.ibb.co/Y2wmC9N/alishalopez.jpg',
+  'https://i.ibb.co/0rbsMmJ/Malorie-on-Twitter.jpg',
+  'https://i.ibb.co/nQf3rfS/Minimal-Classy.jpg',
+];
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [quoteList, setQuoteList] = useState([]);
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -51,6 +61,48 @@ const App = () => {
     </button>
   );
 
+  const sendQuote = async () => {
+    if (inputValue.length > 0) {
+      console.log('Quote link:', inputValue);
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendQuote();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter quote link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-quote-button">
+          Submit
+        </button>
+      </form>
+
+      <div className="quote-grid">
+        {quoteList.map((quote) => (
+          <div className="quote-item" key={quote}>
+            <img src={quote} alt={quote} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
@@ -59,16 +111,24 @@ const App = () => {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching Quote list...');
+      setQuoteList(TEST_QUOTESS);
+    }
+  }, [walletAddress]);
+
   return (
     <div className="App">
       <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="container">
           <div className="header-container">
-            <p className="header">🖼 GIF Portal</p>
+            <p className="header">Take Care Project 💛</p>
             <p className="sub-text">
-              View your GIF collection in the metaverse ✨
+              Inspirational quotes from the metaverse ✨
             </p>
             {!walletAddress && renderNotConnectedContainer()}
+            {walletAddress && renderConnectedContainer()}
           </div>
           <div className="footer-container">
             <img
