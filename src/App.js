8371d1fb83dbf4af12ac3fca7af1e481527cac20
Antonin Nhek <a.nhek@icloud.com>
2021-11-13 17:20:20 +0100

    section 1/completed: building a connect wallet button with Phantom Wallet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -20,6 +22,7 @@ const App = () => {
             'Connected with Public Key:',
             response.publicKey.toString()
           );
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
@@ -29,7 +32,15 @@ const App = () => {
     }
   };
 
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
   const renderNotConnectedContainer = () => (
     <button
@@ -50,22 +61,28 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
-          {renderNotConnectedContainer()}
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+      <div className={walletAddress ? 'authed-container' : 'container'}>
+        <div className="container">
+          <div className="header-container">
+            <p className="header">🖼 GIF Portal</p>
+            <p className="sub-text">
+              View your GIF collection in the metaverse ✨
+            </p>
+            {!walletAddress && renderNotConnectedContainer()}
+          </div>
+          <div className="footer-container">
+            <img
+              alt="Twitter Logo"
+              className="twitter-logo"
+              src={twitterLogo}
+            />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`built on @${TWITTER_HANDLE}`}</a>
+          </div>
         </div>
       </div>
     </div>
